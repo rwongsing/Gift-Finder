919c3d7e505412c474491b8d16e19e836754d696
Robert <rwongsing@yahoo.com>
2020-06-23 11:30:53 -0700

    Added login page and css

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretkey"
+Bootstrap(app)
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+
+    return render_template('login.html', form=form)
